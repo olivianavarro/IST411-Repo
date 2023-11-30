e7da088d34d277b8b5a612cf35609d9de0ba4bee
Olivia Navarro <olivia.navarroalsina@doane.edu>
2023-11-30 09:38:44 -0600

    Alec demo

---
+++ Module 6/MusicApi/Models/MusicItem.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicApi.Models
+{
+    public class MusicItem
+    {
+        public long id { get; set; }
+        public string? songName { get; set; }
+        public string? artistName { get; set; }
+        public int rating { get; set; }
+    }
+}
---
+++ Module 6/MusicApi/wwwroot/js/site.js
@@ -0,0 +1,134 @@
+﻿const uri = 'api/MusicItems';
+let songs = [];
+
+function getItems() {
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => _displayItems(data))
+        .catch(error => console.error('Unable to get items.', error));
+}
+
+function addItem() {
+    const addNameTextbox = document.getElementById('add-name');
+    const addAuthorTextbox = document.getElementById('add-author');
+    const addRatingTextbox = document.getElementById('add-rating');
+
+    const item = {
+        
+        songName: addNameTextbox.value.trim(),
+        artistName: addAuthorTextbox.value.trim(),
+        rating: addRatingTextbox.value.trim()
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(response => response.json())
+        .then(() => {
+            getItems();
+            addNameTextbox.value = '';
+            addAuthorTextbox.value = '';
+            addRatingTextbox.value = "1";
+        })
+        .catch(error => console.error('Unable to add item.', error));
+}
+
+function deleteItem(id) {
+    fetch(`${uri}/${id}`, {
+        method: 'DELETE'
+    })
+        .then(() => getItems())
+        .catch(error => console.error('Unable to delete item.', error));
+}
+
+function displayEditForm(id) {
+    const item = songs.find(item => item.id === id);
+
+    document.getElementById('edit-name').value = item.songName;
+    document.getElementById('edit-author').value = item.artistName;
+    document.getElementById('edit-id').value = item.id;
+    document.getElementById('edit-rating').value = item.rating;
+    document.getElementById('editForm').style.display = 'block';
+}
+
+function updateItem() {
+    const itemId = document.getElementById('edit-id').value;
+    const item = {
+        id: parseInt(itemId, 10),
+        songName: document.getElementById('edit-name').value.trim(),
+        artistName: document.getElementById('edit-author').value.trim(),
+        rating: document.getElementById('edit-rating').value
+    };
+
+    fetch(`${uri}/${itemId}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(() => getItems())
+        .catch(error => console.error('Unable to update item.', error));
+
+    closeInput();
+
+    return false;
+}
+
+function closeInput() {
+    document.getElementById('editForm').style.display = 'none';
+}
+
+function _displayCount(itemCount) {
+    const name = (itemCount === 1) ? 'song' : 'songs';
+
+    document.getElementById('counter').innerText = `${itemCount} ${name}`;
+}
+
+function _displayItems(data) {
+    const tBody = document.getElementById('songs');
+    tBody.innerHTML = '';
+
+    _displayCount(data.length);
+
+    const button = document.createElement('button');
+
+    data.forEach(item => {
+
+        let editButton = button.cloneNode(false);
+        editButton.innerText = 'Edit';
+        editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
+
+        let deleteButton = button.cloneNode(false);
+        deleteButton.innerText = 'Delete';
+        deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
+
+        let tr = tBody.insertRow();
+
+        let td1 = tr.insertCell(0);
+        let textNode = document.createTextNode(item.songName);
+        td1.appendChild(textNode);
+
+        let td2 = tr.insertCell(1);
+        let textNode1 = document.createTextNode(item.artistName);
+        td2.appendChild(textNode1);
+       
+        let td3 = tr.insertCell(2);
+        let textNode2 = document.createTextNode(item.rating);
+        td3.appendChild(textNode2);
+
+        let td4 = tr.insertCell(3);
+        td4.appendChild(editButton);
+
+        let td5 = tr.insertCell(4);
+        td5.appendChild(deleteButton);
+    });
+
+    songs = data;
+}
